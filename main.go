e5b13960e5281dc5f0ddf7e815e877be5eef7cd1
ch0b3 <t.osakaya@raksul.com>
2022-05-05 14:15:38 +0900

    Can decode application/x-www-form-urlencoded

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"log"
+	"net/url"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 // Reference: https://github.com/aws/aws-lambda-go/blob/main/events/lambda_function_urls.go
-func Handler(request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
-	return events.LambdaFunctionURLResponse{
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	body, err := url.QueryUnescape(request.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	str_body := string(body)
+	log.Println(str_body)
+
+	// TODO: bodyからテキストを抽出する
+
+	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body: "OK",
+		Body: "ok",
 	}, nil
 }
 
