9ebbd85499a21517f80025d750ae711ba074c910
ch0b3 <t.osakaya@raksul.com>
2022-05-21 00:58:48 +0900

    Add TestSelectByCount

--- go.mod
+++ go.mod
@@ -6,4 +6,7 @@ require github.com/aws/aws-lambda-go v1.31.1
 
 require github.com/gorilla/websocket v1.4.2 // indirect
 
-require github.com/slack-go/slack v0.10.3
+require (
+	github.com/deckarep/golang-set v1.8.0
+	github.com/slack-go/slack v0.10.3
+)
--- selection/selection_test.go
+++ selection/selection_test.go
@@ -2,6 +2,7 @@ package selection_test
 
 import (
 	"github.com/ch0b3/selector/selection"
+	set "github.com/deckarep/golang-set"
 	"reflect"
 	"testing"
 )
@@ -11,6 +12,32 @@ func TestTextToStruct(t *testing.T) {
 	got := selection.TextToStruct(text)
 	want := selection.Params{Members: []string{"member1", "member2"}, Count: 1}
 	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Fail")
+	}
+}
+
+func TestSelectByCount(t *testing.T) {
+	members := []string{"member1", "member2", "member3"}
+	count := 2
+
+	inputInterfaces := []interface{}{}
+	for _, m := range members {
+		inputInterfaces = append(inputInterfaces, m)
+	}
+
+	params := selection.Params{Members: members, Count: count}
+	gotMembers := selection.SelectByCount(&params)
+
+	gotInterfaces := []interface{}{}
+	for _, m := range gotMembers {
+		gotInterfaces = append(gotInterfaces, m)
+	}
+
+	paramsSet := set.NewSetFromSlice(inputInterfaces)
+	gotSet := set.NewSetFromSlice(gotInterfaces)
+
+	// inputとgotの積集合の数がcountになっていればOK
+	if paramsSet.Intersect(gotSet).Cardinality() != count {
 		t.Errorf("Fail")
 	}
 }
